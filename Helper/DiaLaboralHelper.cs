8baeae6408a8a16949ffc8e1ca6150af4480e640
iotdepartment <iotdepartmenttg@gmail.com>
2025-12-02 23:50:48 -0600

    Cambio martes previo a Login

--- Helper/DiaLaboralHelper.cs
+++ Helper/DiaLaboralHelper.cs
@@ -4,12 +4,12 @@ namespace Batch.Helpers
 {
     public static class DiaLaboralHelper
     {
-        // Devuelve la fecha de inicio del día laboral
-        public static DateTime ObtenerFechaLaboral(DateTime ahora)
+        // Devuelve la fecha de inicio del día laboral (7:00 AM)
+        public static DateTime ObtenerFechaLaboral(DateTime fecha)
         {
-            var inicioDia = new DateTime(ahora.Year, ahora.Month, ahora.Day, 7, 0, 0);
+            var inicioDia = new DateTime(fecha.Year, fecha.Month, fecha.Day, 7, 0, 0);
 
-            if (ahora < inicioDia)
+            if (fecha < inicioDia)
             {
                 // Si estamos antes de las 7:00, pertenece al día laboral anterior
                 inicioDia = inicioDia.AddDays(-1);
@@ -18,17 +18,21 @@ namespace Batch.Helpers
             return inicioDia;
         }
 
-        // Devuelve el turno actual según la hora
-        public static string ObtenerTurno(DateTime ahora)
+        // Devuelve el turno en el que se creó el batch
+        public static string ObtenerTurno(DateTime fecha)
         {
-            var hora = ahora.TimeOfDay;
+            var hora = fecha.TimeOfDay;
 
-            if (hora >= new TimeSpan(7, 0, 0) && hora <= new TimeSpan(15, 30, 0))
-                return "Turno 1";
-            if (hora >= new TimeSpan(15, 31, 0) && hora <= new TimeSpan(23, 44, 0))
-                return "Turno 2";
+            // Turno 1: 07:00 a 15:30
+            if (hora >= new TimeSpan(7, 0, 0) && hora < new TimeSpan(15, 30, 1))
+                return "1";
 
-            return "Turno 3"; // 23:45 a 6:59
+            // Turno 2: 15:30 a 23:45
+            if (hora >= new TimeSpan(15, 30, 0) && hora < new TimeSpan(23, 45, 0))
+                return "2";
+
+            // Turno 3: 23:45 a 06:59
+            return "3";
         }
     }
 }
